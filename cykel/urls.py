31aa0d8be7ba41dd49c8510232ad4ee122c55116
Ex <git@exmatrikulator.de>
2021-01-02 23:14:19 +0100

    serve static files

--- cykel/urls.py
+++ cykel/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("bikesharing/", include("bikesharing.urls")),
     path("gbfs/", include("gbfs.urls")),
     url(r"^auth/", include("allauth.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
